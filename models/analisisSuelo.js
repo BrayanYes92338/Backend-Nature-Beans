bb359cdb979f640a99d4461586de804decc053af
Brayan Duran <brayan.duranx7@gmail.com>
2024-07-09 22:06:23 -0500

    parcelas

--- models/analisisSuelo.js
+++ models/analisisSuelo.js
@@ -4,7 +4,6 @@ const analisisSueloSchema=new mongoose.Schema({
     idParcela:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"Parcela"},
     idEmpleado:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"Empleado"},
     muestra:{type:String,required:true},
-    cultivo:{type:String,required:true},
     laboratorio:{type:String,requerid:true},
     resultados:[{}],
     recomendaciones:{type:String},
