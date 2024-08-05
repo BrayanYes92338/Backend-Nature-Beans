17e7598e8371c42ac1ee22f45988d92e238fc6de
BrayanYes92338 <bycaballero0@soy.sena.edu.co>
2024-08-05 13:30:05 -0500

    Modificaciones en controladores, rutas, helpers de Parcelas

--- controllers/parcela.js
+++ controllers/parcela.js
@@ -1,46 +1,45 @@
-import Parcela from "../models/parcelas.js";
+import Parcela from '../models/parcela.js'
 
-const httpParcela = {
+const httpParcelas = {
   getParcela: async (req, res) => {
-    const parcela = await Parcela.find();
-    res.json({ parcela });
+    const { buscar } = req.query
+    const parcela = await Parcela.find({
+      $or: [{ ubicacion: new RegExp(buscar, "i") }]
+    })
+    .populate({path:'idFinca'})
+    .populate({path:'asistenteTecnico'})
+    res.json({ parcela })
   },
-  getParcelaActiva: async (req, res)=>{
-    const parcelas = await Parcela.find({estado:1})
-    res.json({parcelas})
-    },
-    getParcelaInactiva: async (req ,res)=>{
-        const parcelas = await Parcela.find({estado:0})
-        res.json({parcelas})
-    },
   postParcela: async (req, res) => {
     try {
-      const {idFinca,asistenteTecnico,ubicacion,numero,cultivoAnterior,cultivoActual,detalle,area,estado} = req.body;
-      const parcela = new Parcela({idFinca,asistenteTecnico,ubicacion,numero,cultivoAnterior,cultivoActual,detalle,area,estado});
-
-      await parcela.save();
-      res.json({ parcela });
+      const { idFinca, asistenteTecnico, ubicacion, numero, cultivoAnterior, cultivoActual, detalle, area } = req.body;
+      const parcela = new Parcela({ idFinca, asistenteTecnico, ubicacion, numero, cultivoAnterior, cultivoActual, detalle, area });
+      await parcela.save()
+      res.json({ parcela })
     } catch (error) {
       console.log(error);
-      res.status(400).json({ msg: "Error no se pudo crear la Parcela" });
+      res.status(400).json({ msg: "Error no se pudo crear la parcela" })
     }
   },
-  putParcela: async (req ,res)=>{
-    const {id}=req.params;
+  putParcela: async (req, res)=>{
+    const {id}= req.params;
     const {idFinca, ...resto} = req.body;
-    const parcelas = await Parcela.findByIdAndUpdate(id, {idFinca, ...resto}, {new:true})
-    res.json({parcelas})
-},
-putParcelaActivas: async (req, res) =>{
-  const {id}= req.params;
-  const parcelas = await Parcela.findByIdAndUpdate(id, {estado:1}, {new: true})
-  res.json({parcelas})
-},
-putParcelaInactivas: async (req, res)=>{
-  const {id}= req.params;
-  const parcelas = await Parcela.findByIdAndUpdate(id, {estado:0}, {new: true})
-  res.json({parcelas})
-}
-};
+    const parcela = await Parcela.findByIdAndUpdate(id, {idFinca, ...resto}, {new: true});
+    res.json({parcela})
+  },
+   
+  putParcelaActivar: async (req, res)=>{
+    const {id}= req.params;
+    const parcela = await Parcela.findByIdAndUpdate(id, {estado: 1}, {new: true});
+    res.json({parcela})
+  },
+  putParcelaDesactivar: async (req, res)=>{
+    const {id}= req.params;
+    const parcela = await Parcela.findByIdAndUpdate(id, {estado: 0}, {new: true});
+    res.json({parcela})
+  },
 
-export default httpParcela;
+}
+
+
+export default httpParcelas
