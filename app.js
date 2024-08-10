0ad53b9de17e917b46e098b7bc68d5ce895dbdfb
Brayan Duran <brayan.duranx7@gmail.com>
2024-08-10 18:13:58 -0500

    fertilizacion

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ import comprador from "./routes/comprador.js"
 import mantenimiento from "./routes/mantenimiento.js"
 import inventario from "./routes/invertarios.js"
 import factura from "./routes/factura.js"
+import fertilizacion from "./routes/fertilizacion.js"
 
 
 
@@ -54,6 +55,9 @@ app.use("/api/insumo",insumo)
 app.use("/api/mantenimiento",mantenimiento)
 app.use("/api/inventario",inventario)
 app.use('/api/factura',factura)
+app.use('/api/fertilizacion',fertilizacion)
+
+
 
 
 
---
+++ routes/fertilizacion.js
@@ -0,0 +1,35 @@
+import { Router } from 'express'
+import { check } from 'express-validator'
+import { validarCampos } from '../middleware/validar-campos.js'
+import httpFertilizacion from '../controllers/fertilizacion.js'
+import helperFertilizacion from '../helpers/fertilizacion.js'
+
+
+const router = Router()
+
+router.get('/listar',[],httpFertilizacion.getFertilizacion)
+
+router.get('/listarID/:id',[
+    check('id', 'Se necesita un mongoID que sea valido').isMongoId(),
+    check('id').custom(helperFertilizacion.validarFertilizacionID),
+    validarCampos
+],httpFertilizacion.getFertilizacionID)
+ 
+router.post('/agregar',[
+    check('idCultivo', 'idCultivo no puede estar vacio').notEmpty(),
+    check('idEmpleado', 'idEmpleado no puede estar vacio').notEmpty(),
+    check('idInventario', 'idInventario no puede estar vacio').notEmpty(),
+    check('estadoFenologico', 'estadoFenologico no puede estar vacio').notEmpty(),
+    check('tipo', 'tipo no puede estar vacio').notEmpty(),
+    check('nombreFertilizante', 'nombreFertilizante no puede estar vacio').notEmpty(),
+    check('cantidad', 'cantidad no puede estar vacio').notEmpty(),
+    validarCampos
+],httpFertilizacion.postFertilizacion)
+
+router.put('/editar/:id',[
+    check('id', 'Se necesita un mongoID que sea valido').isMongoId(),
+    check('id').custom(helperFertilizacion.validarFertilizacionID),
+    validarCampos
+],httpFertilizacion.putFertilizacion)
+
+export default router
