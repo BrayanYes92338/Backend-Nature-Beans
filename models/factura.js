6d2a4b4cbbf621ae74eae2dd5cf8301a8f31d68f
Brayan Duran <brayan.duranx7@gmail.com>
2024-10-09 22:30:13 -0500

    factura

--- models/factura.js
+++ models/factura.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
 const facturaSchema = new mongoose.Schema({
-    idInventario:{type:mongoose.Schema.Types.ObjectId,ref:'Inventario',required:true},
-    idComprador:{type:mongoose.Schema.Types.ObjectId,ref:'Comprador',required:true},
-    loteComercialnum:{type:String,required:true},
-    nombreProducto:{type:String,required:true},
-    precio:{type:Number, default:0, required:true},
-    cantidad:{type:Number,required:true},
-    iva:{type:Number,default:0, required:true},
-    subtotal:{type:Number,default:0},
-    total:{type:Number, default:0},
-    fecha:{type:Date,default:Date.now}
-})
+    idComprador: { type: mongoose.Schema.Types.ObjectId, ref: 'Comprador', required: true },
+    loteComercialnum: { type: String, required: true },
+    total: { type: Number, default: 0 },
+    fecha: { type: Date, default: Date.now },
+    detalle: [{
+        idProduccion: { type: mongoose.Schema.Types.ObjectId, ref: 'Comprador', required: true },
+        nombreProducto: { type: String, default: 0},
+        precio: { type: Number, default: 0, required: true },
+        cantidad: { type: Number, required: true }, 
+        iva: { type: Number, default: 0, required: true },
+        subtotal: { type: Number, default: 0 }
+    }]
+});
 
-
-export default mongoose.model("Factura",facturaSchema)
+export default mongoose.model("Factura", facturaSchema);
