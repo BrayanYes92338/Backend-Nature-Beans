6e27e0ad1c198ab5a39d6539a60769154a2436dc
Anamile23 <amacuna@soy.sena.edu.co>
2024-10-15 22:13:25 -0500

    new

--- routes/preparacionSuelo.js
+++ routes/preparacionSuelo.js
@@ -26,7 +26,6 @@ preparacion.post(
   "/agregar",
   [
     check("idParcela", "El ID de la parcela no puede estar vacío").notEmpty(),
-<<<<<<< HEAD
     check("idEmpleadooperario", "El ID del idEmpleadooperario no puede estar vacío").notEmpty(),
 
     check(
@@ -42,53 +41,40 @@ preparacion.post(
       "observaciones",
       "Las observaciones no pueden estar vacías"
     ).notEmpty(),
-=======
-    check("operario", "El ID del operario no puede estar vacío").notEmpty(),
-    check("observaciones","Las observaciones no pueden estar vacías").notEmpty(),
->>>>>>> f8b0006846d9a58160e396d0da8fb884401544f1
+
+    // check("idEmpleadooperario", "El ID del operario no puede estar vacío").notEmpty(),
+    // check("observaciones","Las observaciones no pueden estar vacías").notEmpty(),
+
     validarCampos,
   ],
   httpPreparacionSuelo.postPreparacionSuelo
 );
-// preparacion.post(
-//   "/agregar",
-//   [
-//     check("idParcela", "El ID de la parcela no puede estar vacío").notEmpty(),
-//     check("idEmpleadooperario", "El ID del operario no puede estar vacío").notEmpty(),
-    
-//     // Validaciones para productos
-//     check("productos.*.ingredienteActivo", "El ingrediente activo es requerido").notEmpty(),
-//     check("productos.*.dosis", "La dosis es requerida").notEmpty(),
-//     check("productos.*.metodoAplicacion", "El método de aplicación es requerido").notEmpty(),
-    
-//     // Validación de observaciones
-//     check("observaciones", "Las observaciones no pueden estar vacías").notEmpty(),
-    
-//     validarCampos,
-//   ],
-//   httpPreparacionSuelo.postPreparacionSuelo
-// );
+
 preparacion.put(
   "/editar/:id",
 
   [
-    check("idParcela", "El ID de la parcela no puede estar vacío").notEmpty(),
-    check("idEmpleadooperario", "El ID del idEmpleadooperario no puede estar vacío").notEmpty(),
-    check(
-      "productos.*.ingredienteActivo",
-      "El ingrediente activo es requerido"
-    ).notEmpty(),
-    check("productos.*.dosis", "La dosis es requerida").notEmpty(),
-    check(
-      "productos.*.metodoAplicacion",
-      "El método de aplicación es requerido"
-    ).notEmpty(),
-    check(
-      "observaciones",
-      "Las observaciones no pueden estar vacías"
-    ).notEmpty(),
-    validarCampos,
-    check("id", "Se necesita un mongoid valido").isMongoId(),
+    // validarJWT,
+    check('id', 'Se necesita un mongoID que sea valido').isMongoId(),
+    validarCampos
+
+    // check("idParcela", "El ID de la parcela no puede estar vacío").notEmpty(),
+    // check("idEmpleadooperario", "El ID del idEmpleadooperario no puede estar vacío").notEmpty(),
+    // // check(
+    // //   "productos.*.ingredienteActivo",
+    // //   "El ingrediente activo es requerido"
+    // // ).notEmpty(),
+    // // check("productos.*.dosis", "La dosis es requerida").notEmpty(),
+    // // check(
+    // //   "productos.*.metodoAplicacion",
+    // //   "El método de aplicación es requerido"
+    // // ).notEmpty(),
+    // // check(
+    // //   "observaciones",
+    // //   "Las observaciones no pueden estar vacías"
+    // // ).notEmpty(),
+    // validarCampos,
+    // check("id", "Se necesita un mongoid valido").isMongoId(),
   ],
   httpPreparacionSuelo.putPreparacionSuelo
 );
