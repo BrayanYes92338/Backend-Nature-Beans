53cd25cb233f0cdb15e50ec359e9fc3f552ab891
Anamile23 <amacuna@soy.sena.edu.co>
2024-10-15 20:35:36 -0500

    n

--- routes/preparacionSuelo.js
+++ routes/preparacionSuelo.js
@@ -22,34 +22,34 @@ preparacion.get(
   httpPreparacionSuelo.getResponsables
 );
 
-preparacion.post(
-  "/agregar",
-  [
-    check("idParcela", "El ID de la parcela no puede estar vacío").notEmpty(),
-<<<<<<< HEAD
-    check("idEmpleadooperario", "El ID del idEmpleadooperario no puede estar vacío").notEmpty(),
+// preparacion.post(
+//   "/agregar",
+//   [
+//     check("idParcela", "El ID de la parcela no puede estar vacío").notEmpty(),
+// <<<<<<< HEAD
+//     check("idEmpleadooperario", "El ID del idEmpleadooperario no puede estar vacío").notEmpty(),
 
-    check(
-      "productos.*.ingredienteActivo",
-      "El ingrediente activo es requerido"
-    ).notEmpty(),
-    check("productos.*.dosis", "La dosis es requerida").notEmpty(),
-    check(
-      "productos.*.metodoAplicacion",
-      "El método de aplicación es requerido"
-    ).notEmpty(),
-    check(
-      "observaciones",
-      "Las observaciones no pueden estar vacías"
-    ).notEmpty(),
-=======
-    check("operario", "El ID del operario no puede estar vacío").notEmpty(),
-    check("observaciones","Las observaciones no pueden estar vacías").notEmpty(),
->>>>>>> f8b0006846d9a58160e396d0da8fb884401544f1
-    validarCampos,
-  ],
-  httpPreparacionSuelo.postPreparacionSuelo
-);
+//     check(
+//       "productos.*.ingredienteActivo",
+//       "El ingrediente activo es requerido"
+//     ).notEmpty(),
+//     check("productos.*.dosis", "La dosis es requerida").notEmpty(),
+//     check(
+//       "productos.*.metodoAplicacion",
+//       "El método de aplicación es requerido"
+//     ).notEmpty(),
+//     check(
+//       "observaciones",
+//       "Las observaciones no pueden estar vacías"
+//     ).notEmpty(),
+// =======
+//     check("operario", "El ID del operario no puede estar vacío").notEmpty(),
+//     check("observaciones","Las observaciones no pueden estar vacías").notEmpty(),
+// >>>>>>> f8b0006846d9a58160e396d0da8fb884401544f1
+//     validarCampos,
+//   ],
+//   httpPreparacionSuelo.postPreparacionSuelo
+// );
 
 preparacion.put(
   "/editar/:id",
